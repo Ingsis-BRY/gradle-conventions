6c18d5892bb61a905e8d132088ccbb8720d5a377
Enzo Bersano <bersanoenzo@gmail.com>
2026-09-25 01:58:07 -0300

    fix(settings): move plugin management before plugins
    - move the pluginManagement block before the plugins
    - comply with Gradle settings script ordering requirements

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
-rootProject.name = "gradle-conventions"
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "gradle-conventions"
